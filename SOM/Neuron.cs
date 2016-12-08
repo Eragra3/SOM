e6682cdf2fa5388bbea0f60d25ea12ca3526a6be
Daniel Bider <bider.daniel@gmail.com>
2016-12-08 22:44:37 +0100

    Created initial structure

---
+++ SOM/Neuron.cs
@@ -0,0 +1,18 @@
+﻿using MathNet.Numerics.Distributions;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace SOM
+{
+    public class Neuron
+    {
+        public static IContinuousDistribution ContinuousDistribution { get; set; }
+
+        public Vector<double> Weights { get; set; }
+
+        public Neuron(int weightsCount)
+        {
+            Weights = DenseVector.CreateRandom(weightsCount, ContinuousDistribution);
+        }
+    }
+}
---
+++ SOM/SOM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+
+namespace SOM
+{
+    public class SOM
+    {
+        public LinkedList<Neuron> Neurons { get; set; }
+
+        public SOM(int neuronCount, int weightsCount, double initialWeightsRange)
+        {
+            Neuron.ContinuousDistribution = new ContinuousUniform(-initialWeightsRange, initialWeightsRange);
+
+            for (int i = 0; i < neuronCount; i++)
+            {
+                var neuron = new Neuron(weightsCount);
+                Neurons.AddLast(neuron);
+            }
+        }
+    }
+}
